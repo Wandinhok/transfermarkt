f9a025f16e45787fae61de8456c339c83844b73e
Wandinhok <wandinho2323@gmail.com>
2023-06-15 19:51:13 -0300

    subindo spring

---
+++ transfermarkt/src/main/java/edu/iff/bsi/transfermarkt/TransfermarktApplication.java
@@ -0,0 +1,13 @@
+package edu.iff.bsi.transfermarkt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransfermarktApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransfermarktApplication.class, args);
+	}
+
+}
