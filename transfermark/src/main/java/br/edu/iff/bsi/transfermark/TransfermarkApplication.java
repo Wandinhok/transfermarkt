8bde9c81c75e162cd515865428711011dad03f73
Wandinhok <wandinho2323@gmail.com>
2023-06-15 18:33:39 -0300

    Subindo projeto

---
+++ transfermark/src/main/java/br/edu/iff/bsi/transfermark/TransfermarkApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iff.bsi.transfermark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransfermarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransfermarkApplication.class, args);
+	}
+
+}
---
+++ transfermark/src/test/java/br/edu/iff/bsi/transfermark/TransfermarkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.bsi.transfermark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransfermarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
