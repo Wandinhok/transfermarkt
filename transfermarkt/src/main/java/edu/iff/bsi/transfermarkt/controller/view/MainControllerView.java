696e2ab1025f13707d8f2de96eaff98481bb55a2
Wandinhok <wandinho2323@gmail.com>
2023-06-21 09:20:10 -0300

    Controller

---
+++ transfermarkt/src/main/java/edu/iff/bsi/transfermarkt/controller/view/MainControllerView.java
@@ -0,0 +1,17 @@
+package edu.iff.bsi.transfermarkt.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/")
+public class MainControllerView {
+
+
+    @GetMapping("/")
+    public String page()   {
+        return "index";
+    }
+    
+}
