557e86c738723056e0c2da0c38e94812d553ab91
Wandinhok <wandinho2323@gmail.com>
2023-07-01 19:06:22 -0300

    PostMapping

--- transfermarkt/src/main/java/edu/iff/bsi/transfermarkt/controller/view/MainControllerView.java
+++ transfermarkt/src/main/java/edu/iff/bsi/transfermarkt/controller/view/MainControllerView.java
@@ -2,7 +2,12 @@ package edu.iff.bsi.transfermarkt.controller.view;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.iff.bsi.transfermarkt.entities.User;
+
 
 @Controller
 @RequestMapping(path = "/home")
@@ -13,5 +18,15 @@ public class MainControllerView {
     public String page()   {
         return "index";
     }
+
+    @PostMapping("/new/user")
+    public String registerUser(@ModelAttribute User newuser){
+            System.out.println("User ID: " + newuser.getId());
+            System.out.println("User username: " + newuser.getUsername());
+            System.out.println("User password: " + newuser.getPassword());
+            return "sucess";
+    }
+
+
     
 }
