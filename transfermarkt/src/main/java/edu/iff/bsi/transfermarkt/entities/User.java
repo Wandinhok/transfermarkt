fcb6048b1318a9120418ca09b0538a4ea268478f
Wandinhok <wandinho2323@gmail.com>
2023-07-16 20:04:31 -0300

    h2 database

--- transfermarkt/src/main/java/edu/iff/bsi/transfermarkt/entities/User.java
+++ transfermarkt/src/main/java/edu/iff/bsi/transfermarkt/entities/User.java
@@ -1,7 +1,18 @@
 package edu.iff.bsi.transfermarkt.entities;
 
-public class User {
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class User implements Serializable{
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String username;
     private String password;
